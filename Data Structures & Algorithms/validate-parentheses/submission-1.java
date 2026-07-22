6f6b0ee1037fb3be93d651cb98e8985df83e11a1
Sanjana Gowrishetty <sanjanagowrishetty@gmail.com>
2026-07-22 13:10:00 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                stack.add(c);
+            }
+            else if(!stack.isEmpty() && c==')' && stack.peek()=='('){
+                stack.pop();
+            }
+            else if(!stack.isEmpty() && c=='}' && stack.peek()=='{'){
+                stack.pop();
+            }
+            else if(!stack.isEmpty() && c==']' && stack.peek()=='['){
+                stack.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        if(stack.isEmpty()) return true;
+        return false;
+    }
+}
